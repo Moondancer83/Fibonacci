55632b8c21602a879c44721154c02aafbdcaff6d
Mark Kalinovits <kalee.mark@gmail.com>
2016-06-02 06:23:26 +0200

    working controllers

--- FibonacciWeb/src/main/java/hu/kalee/math/controller/FibonacciController.java
+++ FibonacciWeb/src/main/java/hu/kalee/math/controller/FibonacciController.java
@@ -1,21 +1,34 @@
 package hu.kalee.math.controller;
 
+import hu.kalee.math.lib.IntegerFibonacciNumberCalculator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
- * Created by Marci on 2016.05.31..
+ * Created by Moondancer83 on 2016.05.31..
  */
 @RestController
 public class FibonacciController {
-    @RequestMapping(value = "/math/fibonacci/", method = RequestMethod.GET)
+    @Autowired
+    private IntegerFibonacciNumberCalculator calculator;
+
+    @RequestMapping(value = "/math/fibonacci/info", method = RequestMethod.GET)
     public String info() {
         return "hu.kalee.math.fibonacci";
     }
 
-    @RequestMapping(value = "/math/fibonacci/", method = RequestMethod.GET)
-    public Integer getNthNumber(int index) {
-        return 0;
+    @RequestMapping(value = "/math/fibonacci/{index}", method = RequestMethod.GET)
+    public Integer getNthNumber(@PathVariable(value="index") int index) {
+        return calculator.calculateNthNumber(index);
+    }
+
+    @RequestMapping(value = "/math/fibonacci/list/{index}", method = RequestMethod.GET)
+    public List<Integer> getFirstNthNumber(@PathVariable(value="index") int index) {
+        return calculator.calculateFirstNNumbers(index);
     }
 }
