5b532dc321a81fc90e8f65f166b3ec72edbfabad
Mark Kalinovits <kalee.mark@gmail.com>
2016-06-03 06:15:30 +0200

    Adding custom login page, in memory with DB

---
+++ FibonacciWeb/src/main/java/hu/kalee/math/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package hu.kalee.math.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    public void configureUsers(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password("pwd").roles("USER")
+                .and()
+                .withUser("admin").password("admin_pwd").roles("USER", "ADMIN");
+
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated();
+        http.formLogin().loginPage("/login").permitAll();
+    }
+}
---
+++ FibonacciWeb/src/main/java/hu/kalee/math/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package hu.kalee.math.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
