1c7b8eedc67e438d53f626075fb0d712e1b5d0f6
Mark Kalinovits <kalee.mark@gmail.com>
2016-06-04 06:11:46 +0200

    init

---
+++ FibonacciWeb/src/main/java/hu/kalee/math/config/WebConfig.java
@@ -0,0 +1,17 @@
+package hu.kalee.math.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+//        throw new RuntimeException("Webconfig is running");
+    }
+}
--- FibonacciWeb/src/main/webapp/WEB-INF/jsp/login.jsp
+++ FibonacciWeb/src/main/webapp/WEB-INF/jsp/login.jsp
@@ -11,6 +11,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Moondancer's Math | Login</title>
+
+    <link rel="stylesheet" href="webjars/bootstrap/3.3.6/css/bootstrap.min.css" />
 </head>
 <body>
 
