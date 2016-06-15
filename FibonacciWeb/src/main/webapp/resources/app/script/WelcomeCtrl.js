065512d1ebea1daa1e85540c3bf7d31ab3dd2a63
Mark Kalinovits <kalee.mark@gmail.com>
2016-06-15 23:37:25 +0200

    removing user service from app (YAGNI)

--- FibonacciWeb/src/main/webapp/resources/app/script/WelcomeCtrl.js
+++ FibonacciWeb/src/main/webapp/resources/app/script/WelcomeCtrl.js
@@ -3,20 +3,5 @@
 (function(angular) {
     var core = angular.module('App');
 
-    core.controller('WelcomeCtrl', ['UserService', function (UserService) {
-        var that = this;
-        that.message = null;
-        that.userName = "";
-
-        function setUserName(response) {
-            that.userName = response.data;
-        }
-
-        function showError(response) {
-            that.message = 'Error: Resource response is ' + response.statusText;
-        }
-
-        UserService.getUser(setUserName, showError);
-
-    }])
+    core.controller('WelcomeCtrl', function () {})
 })(angular);
