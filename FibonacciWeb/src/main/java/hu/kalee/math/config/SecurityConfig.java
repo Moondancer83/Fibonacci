2f03560278282af9c521e21202c30864ad7cf315
Mark Kalinovits <kalee.mark@gmail.com>
2016-06-16 05:49:47 +0200

    adding jdbc

--- FibonacciWeb/src/main/java/hu/kalee/math/config/SecurityConfig.java
+++ FibonacciWeb/src/main/java/hu/kalee/math/config/SecurityConfig.java
@@ -1,24 +1,31 @@
 package hu.kalee.math.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    public void configureUsers(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user1").password("pwd").roles("USER")
-                .and()
-                .withUser("admin").password("admin_pwd").roles("USER", "ADMIN");
 
-    }
+    public static final String JDBC_MYSQL_PATH = "jdbc:mysql://mysql.s5.command-center.hu/fibonacci";//"jdbc:mysql://localhost:3306/db1";
+    public static final String USER_NAME = "user1";
+    public static final String PASSWORD = "pass1";
+
+//    @Autowired
+//    public void configureUsers(final AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("admin").password("admin_pwd").roles("USER", "ADMIN");
+//
+//    }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -31,11 +38,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http.authorizeRequests()
-//                .antMatchers("/login**").permitAll()
-//                .antMatchers("/webjars**").permitAll()
-//                .antMatchers("/static**").permitAll()
                 .anyRequest().authenticated();
 
         http.formLogin().loginPage("/login").permitAll();
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource())
+                .usersByUsernameQuery(
+                        "select username,password,enabled from users where username=?")
+                .authoritiesByUsernameQuery(
+                        "select username,authority from authorities where username=?");
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl(JDBC_MYSQL_PATH);
+        dataSource.setUsername(USER_NAME);
+        dataSource.setPassword(PASSWORD);
+
+        return dataSource;
     }
 }
