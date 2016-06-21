ab5a62f439c857e3086f512c2b87ab9281d0e175
Mark Kalinovits <kalee.mark@gmail.com>
2016-06-21 05:23:51 +0200

    fix context path issues

--- FibonacciWeb/src/main/webapp/resources/app/script/FibonacciService.js
+++ FibonacciWeb/src/main/webapp/resources/app/script/FibonacciService.js
@@ -5,11 +5,11 @@
 
     core.factory('FibonacciService', ['$http', function ($http) {
         function getNthNumber(index, successCallback, errorcallback) {
-            $http.get('/math/fibonacci/' + index).then(successCallback, errorcallback);
+            $http.get('math/fibonacci/' + index).then(successCallback, errorcallback);
         }
 
         function getFirstNNumber(index, successCallback, errorcallback) {
-            $http.get('/math/fibonacci/list/' + index).then(successCallback, errorcallback);
+            $http.get('math/fibonacci/list/' + index).then(successCallback, errorcallback);
         }
 
         return {
